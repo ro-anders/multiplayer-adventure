7ca183aea3e04c35f39b05a0dca263496de5e518
Ro Anders <ro.c.anders@gmail.com>
2016-06-15 17:25:27 -0400

    Started StringMap. Attempted POST to server - no luck

--- mac/MacRestClient.cpp
+++ mac/MacRestClient.cpp
@@ -16,14 +16,100 @@
 
 #include "Sys.hpp"
 
+StringMap::StringMap() :
+numEntries(0),
+spaceAllocated(16) {
+    keys = new const char*[spaceAllocated];
+    values = new const char*[spaceAllocated];
+}
+
+StringMap::~StringMap() {
+    for(int ctr=0; ctr<numEntries; ++ctr) {
+        delete[] keys[ctr];
+        delete[] values[ctr];
+    }
+    delete[] keys;
+    delete[] values;
+}
+
+const char* StringMap::get(const char* key) {
+    int foundAt = findIndex(key);
+    const char* found = (foundAt < 0 ? NULL : values[foundAt]);
+    return found;
+}
+
+void StringMap::put(const char* key, const char* value) {
+    int insertInSlot = findIndex(key);
+    if (insertInSlot > 0) {
+        delete[] values[insertInSlot];
+        values[insertInSlot] = copyString(value);
+    } else {
+        if (spaceAllocated == numEntries) {
+            allocateMoreSpace();
+        }
+        keys[numEntries] = copyString(key);
+        values[numEntries] = copyString(value);
+    }
+}
+
+void StringMap::remove(const char* key) {
+    int insertInSlot = findIndex(key);
+    if (insertInSlot > 0) {
+        delete[] keys[insertInSlot];
+        delete[] values[insertInSlot];
+        // Shift everything down
+        memcpy(keys+insertInSlot, keys+insertInSlot+1, numEntries-insertInSlot-1);
+    }
+}
+
+int StringMap::findIndex(const char* key) {
+    int found = -1;
+    for(int ctr=0; (ctr<numEntries) && (found < 0); ++ctr) {
+        if (strcmp(key, keys[ctr])==0) {
+            found = ctr;
+        }
+    }
+    return found;
+}
+
+
 int MacRestClient::makeCall(char* buffer, int bufferLength) {
     
     // Open the HTTP connnection to the server
     const char* serverName = "localhost";
     int port = 9000;
+    // GET from Google
     //const char* message = "GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n";
-    const char* message = "GET /games HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n";
+    // Get list of games
+    //const char* message = "GET /games HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n";
+    // Make a game request
 
+    const char* message = "POST /game HTTP/1.1\r\n"
+                          "Host: localhost\r\n"
+                          "Connection: close\r\n"
+                          "Content-Type: application/x-www-form-urlencoded\r\n"
+                          "Content-Length: 20"
+                          "\r\n"
+                          "ip=1.1.1.1&port=1111\r\n";
+    
+    /*
+    const char* message = "POST /game HTTP/1.1\r\n"
+    "Host: localhost:9000]\r\n"
+    "x-forwarded-for: 9\r\n"
+    "Cache-Control: no-cache\r\n"
+    "Postman-Token: 44085461-0db3-dc92-1e8e-062d2d48ee3a\r\n"
+    "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW\r\n"
+    "\r\n"
+    "----WebKitFormBoundary7MA4YWxkTrZu0gW\r\n"
+    "Content-Disposition: form-data; name=\"ip\"\r\n"
+    "\r\n"
+    "6.6.6.6\r\n"
+    "----WebKitFormBoundary7MA4YWxkTrZu0gW\r\n"
+    "Content-Disposition: form-data; name=\"port\"\r\n"
+    "\r\n"
+    "23\r\n"
+    "----WebKitFormBoundary7MA4YWxkTrZu0gW\r\n";
+    */
     printf("Opening http socket\n");
     int n;
     struct sockaddr_in serv_addr;
