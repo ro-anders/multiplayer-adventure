16b90ec63d80e9946babf21eec836bf8d134bacd
Ro Anders <ro.c.anders@gmail.com>
2016-08-16 23:12:28 -0400

    Removed StringMap

--- engine/RestClient.hpp
+++ engine/RestClient.hpp
@@ -7,38 +7,6 @@
 
 #endif /* RestClient_hpp */
 
-/**
- * A real simple map of strings to strings until we figure out if we need a thrid-party library
- * to handle REST communication
- */
-class StringMap {
-public:
-    
-    StringMap();
-    
-    ~StringMap();
-    
-    const char* get(const char* key);
-    
-    void put(const char* key, const char* value);
-    
-    void remove(const char* key);
-    
-private:
-    int numEntries;
-    
-    int spaceAllocated;
-    
-    const char** keys;
-    const char** values;
-    
-    int findIndex(const char* key);
-    
-    void allocateMoreSpace() {}
-    
-    const char* copyString(const char* source) {return NULL;}
-    
-};
 
 class RestClient {
 public:
