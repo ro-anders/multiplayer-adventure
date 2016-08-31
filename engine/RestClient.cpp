b1b2ceed603c7a437411e84a6e7226b38f159e68
Ro Anders <ro.c.anders@gmail.com>
2016-08-30 20:35:59 -0400

    Changing IP until DNS name is used.

--- engine/RestClient.cpp
+++ engine/RestClient.cpp
@@ -3,7 +3,8 @@
 
 #include <string.h>
 
-const char* RestClient::BROKER_SERVER = "roserver.ddns.net";
+//const char* RestClient::BROKER_SERVER = "roserver.ddns.net";
+const char* RestClient::BROKER_SERVER = "52.35.90.83";
 
 const int RestClient::STUN_PORT = 8888;
 const int RestClient::REST_PORT = 9000;
