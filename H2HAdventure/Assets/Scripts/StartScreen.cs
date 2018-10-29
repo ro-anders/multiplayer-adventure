455f9879ee8e4b98013c8cfef828a52c778b37a0
Ro Anders <ro.c.anders@gmail.com>
2018-10-29 17:41:32 -0400

    Crated opening screen

---
+++ H2HAdventure/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPlayClicked() {
+        Debug.Log("Play Now clicked");
+        SceneManager.LoadScene("Lobby");
+    }
+}
