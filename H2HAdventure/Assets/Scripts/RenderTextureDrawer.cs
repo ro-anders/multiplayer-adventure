676357ed1d96ab1468481c6d17b3d5437fd792bd
Ro Anders <ro.c.anders@gmail.com>
2018-12-11 17:15:49 -0500

    Now displaying clean display in adventure game panel

--- H2HAdventure/Assets/Scripts/RenderTextureDrawer.cs
+++ H2HAdventure/Assets/Scripts/RenderTextureDrawer.cs
@@ -33,29 +33,13 @@ public class RenderTextureDrawer : MonoBehaviour
         //RenderTexture.active = null;
     }
 
-    int at = 0;
     // Update is called once per frame
     void Update()
     {
-        at++;
-        for (int i = 0; i < DRAW_AREA_WIDTH * .2f; i++)
-            for (int j = 0; j < DRAW_AREA_HEIGHT; j++)
-            {
-                texture.SetPixel((at + i), j, new Color(1, 0, 0));
-            }
-        for (int xctr = 0; xctr < DRAW_AREA_WIDTH * .2f; ++xctr)
-        {
-            for (int yctr = 0; yctr < DRAW_AREA_HEIGHT; ++yctr)
-            {
-                int newx = (at + xctr) % DRAW_AREA_WIDTH;
-                red[newx, yctr] = 1;
-            }
-        }
-
         RenderTexture.active = renderTexture;
         //don't forget that you need to specify rendertexture before you call readpixels
         //otherwise it will read screen pixels.
-        texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        //texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
         for (int xctr = 0; xctr < DRAW_AREA_WIDTH; ++xctr)
         {
             for (int yctr = 0; yctr < DRAW_AREA_HEIGHT; ++yctr)
@@ -83,34 +67,34 @@ public class RenderTextureDrawer : MonoBehaviour
         blue[x, y] = color.b;
     }
 
-    //private int at = 0;
-    //public void DemoUpdate()
-    //{
-    //int DRAW_AREA_WIDTH = 320;
-    //int DRAW_AREA_HEIGHT = 256;
-    // int viewWidth = renderTexture.width;
-    //int viewHeight = renderTexture.height;
-    //// Don't draw anything if the drawing space is not big enough.
-    //if ((viewWidth >= DRAW_AREA_WIDTH) && (viewHeight >= DRAW_AREA_HEIGHT))
-    //{
-    //    RenderTexture.active = renderTexture;
+    private int at = 0;
+    public void DemoUpdate()
+    {
+        int DRAW_AREA_WIDTH = 320;
+        int DRAW_AREA_HEIGHT = 256;
+         int viewWidth = renderTexture.width;
+        int viewHeight = renderTexture.height;
+        // Don't draw anything if the drawing space is not big enough.
+        if ((viewWidth >= DRAW_AREA_WIDTH) && (viewHeight >= DRAW_AREA_HEIGHT))
+        {
+            RenderTexture.active = renderTexture;
 
-    //    float stripeWidth = DRAW_AREA_WIDTH * .2f;
-    //    at = (at < DRAW_AREA_WIDTH ? at + 1 : 0);
+            float stripeWidth = DRAW_AREA_WIDTH * .2f;
+            at = (at < DRAW_AREA_WIDTH ? at + 1 : 0);
 
-    //    int drawXStart = (viewWidth - DRAW_AREA_WIDTH) / 2;
-    //    int drawYStart = (viewHeight - DRAW_AREA_HEIGHT) / 2;
+            int drawXStart = (viewWidth - DRAW_AREA_WIDTH) / 2;
+            int drawYStart = (viewHeight - DRAW_AREA_HEIGHT) / 2;
 
-    //    for (int i = 0; i < DRAW_AREA_WIDTH; i++)
-    //        for (int j = 0; j < DRAW_AREA_HEIGHT; j++)
-    //        {
-    //            Color color = ((i >= at) && (i <= at + stripeWidth) ? new Color(0xFF / 256.0f, 0xD8 / 256.0f, 0x4C / 256.0f) : new Color(0, 0, 0));
-    //            texture.SetPixel(drawXStart + i, drawYStart + j, color);
-    //        }
-    //    texture.Apply();
-    //    RenderTexture.active = null; //don't forget to set it back to null once you finished playing with it. 
-    //}
-    //}
+            for (int i = 0; i < DRAW_AREA_WIDTH; i++)
+                for (int j = 0; j < DRAW_AREA_HEIGHT; j++)
+                {
+                    Color color = ((i >= at) && (i <= at + stripeWidth) ? new Color(0xFF / 256.0f, 0xD8 / 256.0f, 0x4C / 256.0f) : new Color(0, 0, 0));
+                    texture.SetPixel(drawXStart + i, drawYStart + j, color);
+                }
+            texture.Apply();
+            RenderTexture.active = null; //don't forget to set it back to null once you finished playing with it. 
+        }
+    }
 
 
 }
