310236e5cbbd4fe3cebae65c2aed65c0117723e5
Ro Anders <ro.c.anders@gmail.com>
2019-01-05 08:24:00 -0500

    All the sounds now working

--- H2HAdventure/Assets/Scripts/AdventureAudio.cs
+++ H2HAdventure/Assets/Scripts/AdventureAudio.cs
@@ -9,6 +9,16 @@ public class AdventureAudio : MonoBehaviour {
 
     public AudioClip putdownClip;
 
+    public AudioClip dragonRoarClip;
+
+    public AudioClip dragonDieClip;
+
+    public AudioClip dragonEatClip;
+
+    public AudioClip glowClip;
+
+    public AudioClip winClip;
+
     public AudioSource speaker;
 
 	public void play (SOUND sound, float volume) {
@@ -20,10 +30,29 @@ public class AdventureAudio : MonoBehaviour {
             case SOUND.PUTDOWN:
                 speaker.clip = putdownClip;
                 break;
+            case SOUND.DRAGONDIE:
+                speaker.clip = dragonDieClip;
+                break;
+            case SOUND.ROAR:
+                speaker.clip =dragonRoarClip;
+                break;
+            case SOUND.EATEN:
+                speaker.clip = dragonEatClip;
+                break;
+            case SOUND.GLOW:
+                speaker.clip = glowClip;
+                break;
+            case SOUND.WON:
+                speaker.clip = winClip;
+                break;
             default:
+                speaker.clip = null;
                 break;
         }
         speaker.volume = volume / MAX.VOLUME;
-        speaker.Play();
+        if (speaker.clip != null)
+        {
+            speaker.Play();
+        }
 	}
 }
