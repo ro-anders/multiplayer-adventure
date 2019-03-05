a3195b4c2af0e246b27bee9344c5be6a698e008d
Ro Anders <ro.c.anders@gmail.com>
2019-03-05 17:10:56 -0500

    New mobile drag approach

--- H2HAdventure/Assets/Scripts/GameScene/AdventureDirectional.cs
+++ H2HAdventure/Assets/Scripts/GameScene/AdventureDirectional.cs
@@ -8,6 +8,10 @@ public class AdventureDirectional : MonoBehaviour
 
 #if UNITY_STANDALONE || UNITY_WEBPLAYER
 #else
+    private bool isDragging = false;
+    private int dragId;
+    private float dragStartX;
+    private float dragStartY;
     private bool joyLeft = false;
     private bool joyRight = false;
     private bool joyUp = false;
@@ -26,19 +30,47 @@ public class AdventureDirectional : MonoBehaviour
 #if UNITY_STANDALONE || UNITY_WEBPLAYER
 #else
         float centerx = Screen.width/2.0f;
-        float centery = Screen.height/2.0f;
         float tan22 = 0.414f;
-        if (Input.touchCount > 0)
+        joyLeft = joyRight = joyUp = joyDown = false;
+        bool gotDrop = false;
+        bool gotDrag = false;
+        for(int ctr=0; ctr<Input.touches.Length; ++ctr)
         {
-            Touch touch = Input.touches[0];
-            float dirx = touch.position.x - centerx;
-            float diry = touch.position.y - centery;
-            joyLeft = (dirx < 0) && (Mathf.Abs(dirx) > Mathf.Abs(diry) * tan22);
-            joyRight = (dirx > 0) && (Mathf.Abs(dirx) > Mathf.Abs(diry) * tan22);
-            joyUp = (diry > 0) && (Mathf.Abs(diry) > Mathf.Abs(dirx) * tan22);
-            joyDown = (diry < 0) && (Mathf.Abs(diry) > Mathf.Abs(dirx) * tan22);
-        } else {
-            joyLeft = joyRight = joyUp = joyDown = false;
+            Touch nextTouch = Input.touches[ctr];
+            // Look for drag
+            if (!gotDrag)
+            {
+                if (isDragging && (nextTouch.fingerId == dragId))
+                {
+                    gotDrag = true;
+                    if (nextTouch.phase == TouchPhase.Began)
+                    {
+                        // Why are we getting a BEGAN twice?
+                        Debug.LogError("Received multiple BEGAN events for finger drag " + dragId);
+                    }
+                    else if ((nextTouch.phase == TouchPhase.Ended) || (nextTouch.phase == TouchPhase.Canceled))
+                    {
+                        // Stopped dragging.  Clear the drag state.
+                        gotDrag = false;
+                        isDragging = false;
+                    }
+                    else
+                    {
+                        float dirx = nextTouch.position.x - dragStartX;
+                        float diry = nextTouch.position.y - dragStartY;
+                        joyLeft = (dirx < 0) && (Mathf.Abs(dirx) > Mathf.Abs(diry) * tan22);
+                        joyRight = (dirx > 0) && (Mathf.Abs(dirx) > Mathf.Abs(diry) * tan22);
+                        joyUp = (diry > 0) && (Mathf.Abs(diry) > Mathf.Abs(dirx) * tan22);
+                        joyDown = (diry < 0) && (Mathf.Abs(diry) > Mathf.Abs(dirx) * tan22);
+                    }
+                } else if ((nextTouch.phase == TouchPhase.Began) && (nextTouch.position.x < centerx))
+                {
+                    gotDrag = true;
+                    isDragging = true;
+                    dragStartX = nextTouch.position.x;
+                    dragStartY = nextTouch.position.y;
+                }
+            }
         }
 #endif
     }
