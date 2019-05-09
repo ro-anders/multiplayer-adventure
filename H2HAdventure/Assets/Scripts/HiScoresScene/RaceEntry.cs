a936367c64846a8fbea46294f5a71f0aa7c3b7ab
Ro Anders <ro.c.anders@gmail.com>
2019-05-09 10:28:42 -0400

    Got leaderboard working but not all stages

--- H2HAdventure/Assets/Scripts/HiScoresScene/RaceEntry.cs
+++ H2HAdventure/Assets/Scripts/HiScoresScene/RaceEntry.cs
@@ -9,6 +9,7 @@ public class RaceEntry : MonoBehaviour
     public Text nameText;
     public Text dateText;
 
+
     public string Name
     {
         set { nameText.text = value; }
@@ -17,8 +18,14 @@ public class RaceEntry : MonoBehaviour
     {
         set
         {
-            DateTime date = new DateTime(value);
-            dateText.text = date.ToString("MMM dd, yyyy");
+            DateTime date = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            UnityEngine.Debug.Log("Epoch is " + date.ToString("MMM dd, yyyy"));
+            date = date.AddSeconds(value);
+            UnityEngine.Debug.Log("Timestamp is " + date.ToString("MMM dd, yyyy"));
+            date = date.ToLocalTime();
+            UnityEngine.Debug.Log("Local Timestamp is " + date.ToString("MMM dd, yyyy"));
+            UnityEngine.Debug.Log("Got timestamp " + value + " which is " + date.ToString("MMM dd, yyyy"));
+            dateText.text = date.ToString("MMM d, yyyy");
         }
     }
 }
