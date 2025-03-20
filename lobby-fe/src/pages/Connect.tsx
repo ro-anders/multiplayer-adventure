6a1ff3a488e0148e50d0c6fecb5e8ebe59175179
Ro Anders <ro.c.anders@gmail.com>
2025-03-20 17:05:54 -0400

    Made a tunnel that moves because I have better things to do

--- lobby-fe/src/pages/Connect.tsx
+++ lobby-fe/src/pages/Connect.tsx
@@ -18,15 +18,42 @@ interface ConnectProps {
 function ConnectPage({username}: ConnectProps) {
 
   let [selected, setSelected] = useState<string>('none');
+  let [pathPosition, setPathPosition] = useState<number>(1);
+
+  const pathPositions: { [key: string]: number } = {}
+  pathPositions['schedule'] = 10;
+  pathPositions['call'] = 1.8;
+  pathPositions['notify'] = 0.56;
+  pathPositions['engage'] = 0.1;
+
+  /**
+   * React when an action is chosen.
+   * @param chosen the string code for the chosen action
+   */
+  function onSelected(chosen: string) {
+    setSelected(chosen)
+    setPathPosition(pathPositions[chosen])
+
+  }
   
   return (
     <div className="connect-page">
       <TitleBar/>
+      <div className="connect-separator-column">
+        <div className="connect-separator-wall"/>
+        <div className="connect-separator-path"/>
+        <div className="connect-separator-wall"/>
+      </div>
       <div className="connect-room">
-        <div onClick={()=>{setSelected('schedule')}}>Join a scheduled event</div>
-        <div onClick={()=>{setSelected('call')}}>Send out a call</div>
-        <div onClick={()=>{setSelected('notify')}}>Get notified</div>
-        <div onClick={()=>{setSelected('engage')}}>Reach out</div>
+        <div onClick={()=>{onSelected('schedule')}}>Join a scheduled event</div>
+        <div onClick={()=>{onSelected('call')}}>Send out a call</div>
+        <div onClick={()=>{onSelected('notify')}}>Get notified</div>
+        <div onClick={()=>{onSelected('engage')}}>Reach out</div>
+      </div>
+      <div className="connect-separator-column">
+        <div className="connect-separator-wall"/>
+        <div className="connect-separator-path"/>
+        <div style={{flex: pathPosition}} className="connect-separator-wall"/>
       </div>
       <div className="connect-room">
         {(selected === 'schedule') &&
@@ -44,6 +71,11 @@ function ConnectPage({username}: ConnectProps) {
         {(selected === 'engage') &&
           <div>You must know someone who will like this. ...</div>
         }
+      </div>
+      <div className="connect-separator-column">
+        <div className="connect-separator-wall"/>
+        <div className="connect-separator-path"/>
+        <div className="connect-separator-wall"/>
       </div>
     </div>
   );
